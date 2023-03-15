1bbabdd6335c42d06f4c1ac21f80dab025b5ec05
Judisoft <kumjude09@gmail.com>
2023-03-15 12:29:29 +0000

    counting command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let arguments = process.argv.length;
-if (arguments < 3) {
+const cmdArguments = process.argv.length;
+if (cmdArguments < 3) {
   console.log('No argument');
-}
-else if (arguments === 3) {
+} else if (cmdArguments === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
