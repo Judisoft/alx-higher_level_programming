f752893163c3143d84e331797c9db1eff4a8bd65
Judisoft <64473411+Judisoft@users.noreply.github.com>
2023-03-15 21:48:47 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const cmdArguments = process.argv.length;
-if (cmdArguments < 3) {
-  console.log('No argument');
-} else if (cmdArguments === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
