673a1015d3e6f19d1e52d3b9f43199f359d650c7
Judisoft <kumjude09@gmail.com>
2023-04-25 23:34:08 +0000

    write file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (error, content) => console.log(error || 'File write success')); 
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
+  if (error) console.log(error)
+}); 
